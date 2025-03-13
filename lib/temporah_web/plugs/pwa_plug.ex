91f81ce0c0a4f544cbe5b592a6c560805a30005b
Matan Amidor <matan@smarttrackers.nl>
2025-03-13 17:12:57 +0100

    Fix pwa plug

--- lib/temporah_web/plugs/pwa_plug.ex
+++ lib/temporah_web/plugs/pwa_plug.ex
@@ -1,25 +1,30 @@
-def require_pwa(conn, _opts) do
-  # Check for the cookie
-  pwa_installed_cookie = conn.cookies["pwa_installed"]
+defmodule TemporahWeb.PwaPlugs do
+  import Plug.Conn
+  import Phoenix.Controller
 
-  # Also look for a special query param that could be set via JS redirect
-  # if cookies fail but localStorage works
-  pwa_param = conn.query_params["pwa_installed"]
+  def require_pwa(conn, _opts) do
+    # Check for the cookie
+    pwa_installed_cookie = conn.cookies["pwa_installed"]
 
-  cond do
-    # Skip check for installation page itself
-    String.starts_with?(conn.request_path, "/install") ||
-        String.starts_with?(conn.request_path, "/assets") ->
-      conn
+    # Also look for a special query param that could be set via JS redirect
+    # if cookies fail but localStorage works
+    pwa_param = conn.query_params["pwa_installed"]
 
-    # PWA is installed according to cookie or param
-    pwa_installed_cookie == "true" || pwa_param == "true" ->
-      conn
+    cond do
+      # Skip check for installation page itself
+      String.starts_with?(conn.request_path, "/install") ||
+          String.starts_with?(conn.request_path, "/assets") ->
+        conn
 
-    # Default case: redirect to install
-    true ->
-      conn
-      |> redirect(to: "/install")
-      |> halt()
+      # PWA is installed according to cookie or param
+      pwa_installed_cookie == "true" || pwa_param == "true" ->
+        conn
+
+      # Default case: redirect to install
+      true ->
+        conn
+        |> redirect(to: "/install")
+        |> halt()
+    end
   end
 end
