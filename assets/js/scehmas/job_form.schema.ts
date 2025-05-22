a119bd8c1ce9dd57645087393b2d8efeea3eecd8
Matan Amidor <matan@smarttrackers.nl>
2025-05-23 00:00:23 +0200

    Fine grain jobs schema

--- assets/js/scehmas/job_form.schema.ts
+++ assets/js/scehmas/job_form.schema.ts
@@ -6,15 +6,15 @@ import { m } from '$paraglide/messages'
 import { formLocationSchema } from './api/google_maps'
 
 export const jobFormSchema = v.object({
-  title: v.pipe(v.string(), v.nonEmpty(m.add_job_title_required())),
+  title: v.pipe(v.string(), v.nonEmpty(m['add_job.job_title.missing']())),
   location: v.optional(formLocationSchema),
   startDate: v.pipe(
     v.string(),
-    v.nonEmpty(m.add_job_start_date_required()),
+    v.nonEmpty(m['add_job.date.start.missing']()),
     v.custom<string>(input => {
       const date = parseDate(input as string)
       return date.compare(today('UTC')) > 0
-    }, m.add_job_start_date_before_today()),
+    }, m['add_job.date.start.in_past']()),
   ),
   endDate: v.string(),
 })
