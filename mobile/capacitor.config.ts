b0a44b5b71d8700efb245d5684c491b6b9f990f1
Matan Amidor <matan@smarttrackers.nl>
2025-05-26 20:47:18 +0200

    Move mobile builds to their own folder

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -1,13 +1,39 @@
-import type { CapacitorConfig } from '@capacitor/cli'
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.example.tempi',
-  appName: 'Tempi',
-  webDir: '../priv/static/',
-  server: {
-    url: 'http://localhost:4000',
-    cleartext: true,
-  },
-}
+  appId: "com.example.tempi",
+  appName: "Tempi",
+  webDir: "../priv/static/",
 
-export default config
+  server: {
+    url: "http://localhost:4000",
+    cleartext: true,
+    hostname: "localhost",
+    allowNavigation: [
+      "https://maps.googleapis.com",
+      "https://places.googleapis.com",
+    ],
+  },
+
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+  },
+
+  ios: {
+    contentInset: "automatic",
+  },
+
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+    Keyboard: {
+      resize: "body",
+      style: "dark",
+      resizeOnFullScreen: true,
+    },
+  },
+};
+
+export default config;
